efe3f072c32a8a32113777dde6a59916d8bd8b7e
Nikolay Aleksandrenko <nikolay.aleksandrenko@siteground.com>
2022-02-22 19:13:10 +0200

    Adding some boilerplate in the page page

--- src/pages/AppWrapper/AppWrapper.tsx
+++ src/pages/AppWrapper/AppWrapper.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Link, useParams, Outlet, NavLink} from "react-router-dom";
 import {ROUTES} from "../../Router";
 
+import './style.css';
+
 const AppWrapper = () => {
     let params = useParams();
 
     return (
-        <div>
+        <div className="app">
             <div style={{background: 'gray', padding: 10, color: '#fff'}}>
                 <Link to={`/${ROUTES.APPS}`}>Clear</Link>
                 <b>{params.appId}</b>
@@ -14,6 +16,16 @@ const AppWrapper = () => {
                 <NavLink to={ROUTES.PAGES}>Pages</NavLink>
                 <NavLink to={ROUTES.DATA}>Data</NavLink>
                 <NavLink to={ROUTES.ASSETS}>Assets</NavLink>
+
+                undo / redo
+
+                <input placeholder="example command: Add input in #modal1" />
+
+                version: 0.01
+                <button>save</button>
+                <button>⋮</button>
+                (user)
+
             </div>
             <Outlet />
         </div>
--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 
+import './styles.css';
+
 
 const Page = () => {
     let params = useParams();
 
     return (
-        <div>
-            <div style={{background: 'gray', padding: 10, color: '#fff'}}>
-                page
-                <h1>Page: {params.pageId}</h1>
+    <div className="page">
+
+
+        <div className="page-tools">
+
+            Tree / Libs / Data
+            <input placeholder="search" />
+        </div>
+
+        <div className="page-preview">
+            <div className="page-preview--tools">
+                <div>alex (project) - dashboard (page)</div>
+                <div>screensize: 1200/800 | zoom: 80%</div>
+            </div>
+
+            <div className="page-preview--page-space">
+                <div className="page-preview--page">
+                    the page is rendered here
+                </div>
             </div>
         </div>
+
+        <div className="page-properties">
+            selected component/data process goes here
+        </div>
+
+        <div className="page-actions">
+            Actions module goes here
+            To define the user interactions with the ui
+        </div>
+    </div>
     )
 }
 
