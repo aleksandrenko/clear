f2ac8703b42cee2e473c8f50f686d0fcf0bebe76
Nikolay Aleksandrenko <nikolay.aleksandrenko@siteground.com>
2022-02-24 03:12:12 +0200

    Simple components usage

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -4,6 +4,69 @@ import {useParams} from "react-router-dom";
 import './styles.css';
 
 
+const HTML_COMPONENTS = [
+    {
+        name: 'div',
+        render: (props: any) => <div {...props}>DIV: { props?.children || null }</div>,
+        props: {},
+        descriptions: 'component description user entered',
+        childrenRestrictions: {},
+        actions: {},
+    },
+    {
+        name: 'span',
+        render: (props: any) => <span {...props}>span: { props?.children || null }</span>,
+        props: {},
+        descriptions: 'component description user entered',
+        childrenRestrictions: {},
+        actions: {},
+    },
+    {
+        name: 'Image',
+        render: (props: any) => <img {...props}>{ props?.children || null }</img>,
+        props: {},
+        descriptions: 'component description user entered',
+        childrenRestrictions: {},
+        actions: {},
+    },
+    {
+        name: 'Input',
+        render: (props: any) => <input {...props}>{ props?.children || null }</input>,
+        props: {},
+        descriptions: 'component description user entered',
+        childrenRestrictions: {},
+        actions: {},
+    }
+];
+
+const LIBRARIES = [
+    {
+        name: "HTML Components",
+        components: HTML_COMPONENTS
+    }
+];
+
+const PAGE_COMPONENTS = [
+    {
+        uuid: 'UUIDForTheElement',
+        name: 'Container_1',
+        component: 'div', //TODO: link them by id or something
+        props: {},
+        actions: {},
+        children: [
+            'uuidSpan'
+        ],
+    },
+    {
+        uuid: 'uuidSpan',
+        name: 'Container_2',
+        component: 'span',
+        props: {},
+        actions: {},
+        children: [],
+    }
+]
+
 const Page = () => {
     let params = useParams();
 
@@ -13,6 +76,37 @@ const Page = () => {
 
             Tree / Libs / Data
             <input placeholder="search" />
+
+            <h4>// Tree //</h4>
+
+            {
+                PAGE_COMPONENTS.map((component) => {
+                    return (
+                        <div>{component.name} ({component.component})</div>
+                    );
+                })
+            }
+
+            <hr />
+
+            <h4>// Libs //</h4>
+            {
+                LIBRARIES.map((lib) => {
+                   return (
+                       <div>
+                           <strong>{lib.name}</strong>
+                            <div>
+                                {lib.components.map((component) => {
+                                    return (
+                                        <div>{ component.name }</div>
+                                    )
+                                })}
+                            </div>
+                       </div>
+                   )
+                })
+            }
+
         </div>
 
         <div className="page-preview">
